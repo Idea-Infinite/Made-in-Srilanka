e794a23a366473177e077b7d718dfba1939744ea
vikumkbv <vikumkbv@gmail.com>
2020-12-22 17:56:18 +0530

    ipad aboutus page complete

--- ipad/pages/about.php
+++ ipad/pages/about.php
@@ -4,31 +4,34 @@
     <?php include '../parts/header.php' ?>
 
     <div role="main" class="ui-content">
+
         <div class="header-title">
             <h3>About Us</h3>
             <img src="../../common/assets/images/icons/wishList.png" height="35px" width="35px">
         </div>
-        <p style="font-size: 12px; font-family: 'Montserrat', sans-serif; text-align: justify">
-            iCraft is the leading independent home improvement and home repair website. It has been repeatedly honored
-            among the Best on the Web, and was named "One of the Top 50 Sites in the World" by Time Magazine. In
-            addition to our broad list of topics, we operate the most active home improvement forum on the internet,
-            enabling consumers to get personalized advice from professionals in over 100 subjects.
-
-            iCraft was established in 1995 with the goal of providing consumers with unbiased home improvement and
-            repair information in a community environment. Today DoItYourself averages over ten million unique visitors
-            per month. With comprehensive how-to content, engaging features and interviews, and expertly moderated
-            community forums, DoItYourself makes it easy and fun to tackle even the most complex home improvement
-            projects.
-
-            Would you like to share a project you DIYed? Check out our Projects section.
-
-
-        </p>
         <center>
-            <img src="../../common/assets/images/aboutus.png" height="180px" width="340px">
+            <p style="font-size: 14px; font-family: 'poppins', serif; text-align: justify; width: 700px">
+                iCraft is the leading independent home improvement and home repair website. It has been repeatedly honored
+                among the Best on the Web, and was named "One of the Top 50 Sites in the World" by Time Magazine. In
+                addition to our broad list of topics, we operate the most active home improvement forum on the internet,
+                enabling consumers to get personalized advice from professionals in over 100 subjects.
+                <br /><br />
+                iCraft was established in 1995 with the goal of providing consumers with unbiased home improvement and
+                repair information in a community environment. Today DoItYourself averages over ten million unique visitors
+                per month. With comprehensive how-to content, engaging features and interviews, and expertly moderated
+                community forums, DoItYourself makes it easy and fun to tackle even the most complex home improvement
+                projects.
+                <br /><br />
+                Would you like to share a project you DIYed? Check out our Projects section.
+
+
+            </p>
+        
+            <img src="../../common/assets/images/aboutus.png" style="width: 700px; height: 267px; left: 249px; top: 480px;">
         </center>
     </div>
     <?php include '../parts/bottomNavbar.php' ?>
-</div><?php include '../parts/footer.php' ?>
+    <?php include '../parts/footer.php' ?>
+</div>
 </body>
 </html>
