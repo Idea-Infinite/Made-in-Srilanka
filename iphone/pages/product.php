c063ad8c52b77b9b15ac2c0fcb6604cc2f34c782
Charith Jayasanka <charithjayasanka2@gmail.com>
2020-12-23 22:51:04 +0530

    map data to product

--- iphone/pages/product.php
+++ iphone/pages/product.php
@@ -19,7 +19,7 @@
         <div class="ui-bar ui-bar-a ui-card" style="height: unset; max-width: 70%; margin: auto;">
 
             <span class="flaticon-122-heart fav-icon-card"></span>
-            <img class="center" src="../images/cat.png" style="max-width: 120px">
+            <?php echo '<img class="center" src="', $json[$id]['image'], '" style="max-width: 120px">' ?? '' ?>
             <div class="ui-grid-a">
                 <div class="ui-block-a" style="width: 80%">
                     <p style="font-weight: 200; font-family: 'Poppins';"><?php echo $json[$id]['name'] ?? '' ?> </p>
@@ -148,9 +148,9 @@
                 data = [];
             }
             if (!found) {
-                data.push({"id": <?php echo $id ?>, "qty": qty});
+                data.push({ "id": <?php echo $id ?>, "qty": qty });
             }
-            $.cookie('cart', JSON.stringify(data), {path: '/'});
+            $.cookie('cart', JSON.stringify(data), { path: '/' });
             $('#addToCart').html('Added ' + qty);
             alert("Added to the cart");
         });
