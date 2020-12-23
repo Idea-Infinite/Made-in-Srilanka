a3ec35d2154ccf1323917e2206f07cf0425c8129
Gigara <info@gigara.info>
2020-12-23 23:23:06 +0530

    Fix poi image not showing issue.

--- iphone/pages/shop.php
+++ iphone/pages/shop.php
@@ -28,6 +28,7 @@
                 }
                 $name = $item['name'];
                 $price = $item['price'];
+                $image = $item['image'];
                 include '../parts/poiCard.php';
             }
             ?>
--- iphone/parts/poiCard.php
+++ iphone/parts/poiCard.php
@@ -1,10 +1,10 @@
 <div class="ui-block-<?php echo $column ?>  card ui-corner-all custom-corners">
     <a href="product.php?id=<?php echo $key ?>">
         <div class="ui-bar ui-bar-a ui-card">
-        <span class="flaticon-122-heart fav-icon-card"></span>
-        <img class="center" src="../images/cat.png" width="80" height="80"></br>
+            <span class="flaticon-122-heart fav-icon-card"></span>
+            <img class="center" src="<?php echo $image ?>" width="80" height="80"></br>
             <p class="card-title"><?php echo $name ?> </p> </br> <span
                     class="card-price"><?php echo $price ?? '' ?></span>
-    </div>
+        </div>
     </a>
 </div>
