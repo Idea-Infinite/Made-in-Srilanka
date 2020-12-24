c6a1179963e7ad437ab77425f8c904895a33b451
Charith Jayasanka <charithjayasanka2@gmail.com>
2020-12-24 21:05:27 +0530

    customer rewards

--- iphone/popups/customer_Rewards.php
+++ iphone/popups/customer_Rewards.php
@@ -1,14 +1,14 @@
-<div style="max-width: none; min-width: 300px;" data-role="popup" id="customer_Rewards" data-theme="a" class="ui-content">
-    <div class="header-title" style="margin-top: -10px; display: flex">
-        <h3>Chat with seller</h3>
-        <img src="../../common/assets/images/icons/contact_us.png" height="35px" width="35px">
+<div style="max-width: none; min-width: 300px;" data-role="popup" id="customer_Rewards" data-theme="a"
+     class="ui-content">
+    <div class="header-title" style="margin-top: -10px; margin: 10px; display: flex">
+        <h3>You are awarded with</h3>
     </div>
-    <div class="chat-with-seller">
+    <div style="display: flex; justify-content: center;">
+        <img src="../../common/assets/images/diamonds.png" class="chat-with-seller">
     </div>
-    <form>
-        <div class="chat-box">
-            <input type="text" name="username" id="username" value="smith">
-            <img style="float: right; margin-top: -37px; margin-right: 15px;" src="../../common/assets/images/send.png">
-        </div>
-    </form>
+    <br>
+    <div style="display: inline-flex">
+        <img style="height: 30px; margin: 10px" src="../../common/assets/images/diamond-icon.png" class="chat-with-seller">
+        <p>You are awarded with 4 diamonds</p>
+    </div>
 </div>
