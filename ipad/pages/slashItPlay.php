8b9a682a619212514d2a5ecda0e32f61861fd1c5
Charith Jayasanka <charithjayasanka2@gmail.com>
2020-12-26 02:30:07 +0530

    fix slashit

--- ipad/pages/slashItPlay.php
+++ ipad/pages/slashItPlay.php
@@ -1,33 +1,31 @@
 <?php include '../parts/head.php' ?>
 <!-- This is slash it page-->
 <div data-role="page" id="slashit">
-    <?php include '../parts/header.php' ?>
-    <?php
-    $data = $GLOBALS['db'];
-    $json = json_decode($data, true);
-    $id = $_GET['id'];
-    ?>
+
     <div role="main" class="ui-content">
         <center>
             <img src="../../common/assets/images/slashit/select_product.png"
-                 style="margin: -20px -20px 20px -10px; width: 110%;">
+                 style="margin: -20px -20px 20px -10px; width: 70%;">
         </center>
 
-        <?php
-        $data = $GLOBALS['db'];
-        $json = json_decode($data, true);
+        <div>
+            <?php
+            $data = $GLOBALS['db'];
+            $json = json_decode($data, true);
 
-        foreach ($json as $key => $item) {
-            if ($key % 2 == 0) {
-                $column = 'a';
-            } else {
-                $column = 'b';
+            foreach ($json as $key => $item) {
+                if ($key % 2 == 0) {
+                    $column = 'a';
+                } else {
+                    $column = 'b';
+                }
+                $name = $item['name'];
+                $price = $item['price'];
+                $image = $item['image'];
+                include '../parts/poiCard.php';
             }
-            $name = $item['name'];
-            $price = $item['price'];
-            include '../parts/poiCard.php';
-        }
-        ?>
+            ?>
+        </div>
 
         <?php include '../parts/bottomNavbar.php' ?>
     </div><?php include '../parts/footer.php' ?>
