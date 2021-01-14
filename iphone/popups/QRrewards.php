aa5d3ef65ed0720d070f7a9181767067efcb34b7
Charith Jayasanka <charithjayasanka2@gmail.com>
2021-01-15 03:15:21 +0530

    fix alignment

--- iphone/popups/QRrewards.php
+++ iphone/popups/QRrewards.php
@@ -1,7 +1,7 @@
 <div style="max-width: none; min-width: 300px;" data-role="popup" id="qrRewards" data-theme="a"
      class="ui-content">
     <div class="header-title" style="margin-top: -10px; margin: 10px; display: flex">
-        <h3>You are awarded with</h3>
+        <h3 style="text-align: center">Hooray !! Here's your reward</h3>
     </div>
     <div style="display: flex; justify-content: center;">
         <img src="../../common/assets/images/diamonds.png" class="chat-with-seller">
@@ -10,6 +10,6 @@
     <div style="display: inline-flex">
         <img style="height: 30px; margin: 10px" src="../../common/assets/images/diamond-icon.png"
              class="chat-with-seller">
-        <p>You are awarded with <span id="qrId"></span></p>
+        <p style="text-align: center">You are awarded with <span id="qrId"></span></p>
     </div>
 </div>
