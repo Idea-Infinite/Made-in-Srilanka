ff6dc39404ed2cb88499148d2c07acf07f6fb747
Chamupathi Gigara Hettige <info@gigara.info>
2021-09-12 23:55:07 +0530

    Update vars.php

--- common/vars.php
+++ common/vars.php
@@ -2,7 +2,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 $GLOBALS['db'] = file_get_contents('..' . DS . '..' . DS . 'common' . DS . 'db.json');
 $GLOBALS['sodb'] = file_get_contents('..' . DS . '..' . DS . 'common' . DS . 'sodb.json');
-$GLOBALS['stripe_key'] = "pk_test_51Hs4vICZjSyoKagriy62PgWm6qQLhrJtIYyy3Lq4GWCGNodf82TR4SFuLY4J4mcjNX45Kf7Yfjg80dv665AMmzK400rmoSi33N";
+$GLOBALS['stripe_key'] = "API_KEY_HERE";
 $GLOBALS['domain'] = "http://localhost/Made-in-Srilanka/";
 if (!isset($_COOKIE['diamondCount'])) {
     setcookie('diamondCount', 10, time() + (86400 * 30), "/");
